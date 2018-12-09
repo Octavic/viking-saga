98f5d08410f3e67405914da5061d6dc5a009579b
Octavic <yifeixu@live.com>
2018-12-09 13:00:28 -0800

    Add scrolling

---
+++ Assets/Scripts/MainCamera.cs
@@ -0,0 +1,26 @@
+﻿namespace Assets.Scripts
+{
+	using System.Collections;
+	using System.Collections.Generic;
+	using UnityEngine;
+
+	public class MainCamera : MonoBehaviour
+	{
+
+		// Use this for initialization
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+			var targetPos = PlayerController.CurrentInstance.transform.position;
+			targetPos.x = Mathf.Max(targetPos.x, 0);
+			targetPos.y = 0;
+			targetPos.z = -10;
+			this.transform.position = targetPos;
+		}
+	}
+}
